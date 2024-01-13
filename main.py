1e9d4db81b0727d5f9532055440dc647e492549d
PirATesofArabian <62105014+PirATesofArabian@users.noreply.github.com>
2024-01-13 11:55:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ elif action == "Update Existing Vendor":
 
 # View All Vendors
 elif action == "View Vendor Data":
-    existing_data["InvoiceNumber"]=existing_data["InvoiceNumber"].astype(int)
+    existing_data["InvoiceNumber"]=existing_data["InvoiceNumber"].astype(str)
     df=pd.DataFrame(existing_data)
     filters=["InvoiceNumber","VendorName","Amount","InvoiceDate","AmountPaid"]
     dynamic_filters = DynamicFilters(df,filters=["InvoiceNumber","VendorName","Amount","InvoiceDate","AmountPaid"])
