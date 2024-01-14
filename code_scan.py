f0c389552094ad584b2de31720abf8c391a9a370
yash <yashwanthbs55@gmail.com>
2024-01-14 14:33:23 +0530

    changes

--- code_scan.py
+++ code_scan.py
@@ -1,6 +1,7 @@
 import cv2
 from pyzbar.pyzbar import decode
 import time
+import pandas as pd
 # import streamlit as st
 
 
@@ -8,6 +9,8 @@ cap = cv2.VideoCapture (0)
 cap.set (3, 640) #3 Width
 cap. set (4, 480) #4 Height
 used_codes = []
+df=pd.DataFrame(columns=['Barcode','IN/OUT'])
+print(df)
 camera = True
 # button=st.button(label="Click to enter the data")
 while camera == True:
@@ -15,7 +18,6 @@ while camera == True:
     for code in decode (frame):
         if code.data.decode('utf-8') not in used_codes:
             print('Approved. You can enter!')
-            st.write("approved")
             print (code.data.decode ('utf-8'))
             used_codes.append (code.data.decode ('utf-8'))
             time.sleep (5)
@@ -24,5 +26,8 @@ while camera == True:
             time.sleep (5)
         else:
             pass
+    df["Barcode"]=used_codes
+    print(df)
     cv2.imshow ('Testing-code-scan', frame)
     cv2.waitKey (1)
+    cv2.destroyAllWindows()
