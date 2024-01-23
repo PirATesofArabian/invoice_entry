cd37fab11f3757a4f9b0eba8d05ab0bc58a91d2e
PirATesofArabian <62105014+PirATesofArabian@users.noreply.github.com>
2024-01-23 23:43:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-import pandas as pd
-import gspread
-from google.oauth2.service_account import Credentials
-from pyzbar.pyzbar import decode
-import streamlit as st
-import time
 import cv2
+import gspread
+import time
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.camera import Camera
+from pyzbar.pyzbar import decodez
+from google.oauth2.service_account import Credentials
 
 def scan_barcode():
     cap = cv2.VideoCapture(0)
     barcode_scanner = cv2.QRCodeDetector()
-    used_codes = []
-
+    used_codes=[]
     while True:
-        success, frame = cap.read()
-        for code in decode(frame):
+        success, frame = cap.read ()
+        for code in decode (frame):
             if code.data.decode('utf-8') not in used_codes:
-                st.success('Approved. You can enter!')
-                data = code.data.decode('utf-8')
-                barcode_data = {'Barcode': data}
-                st.write(barcode_data)
-                used_codes.append(code.data.decode('utf-8'))
-                time.sleep(5)
+                print ('Approved. You can enter!')
+                data=code.data.decode ('utf-8')
+                barcode_data={'Barcode': data}
+                print (barcode_data)
+                used_codes.append (code.data.decode('utf-8'))
+                return barcode_data
+                time.sleep (5)
             elif code.data.decode('utf-8') in used_codes:
-                st.warning('Sorry, this code has already been used')
-                time.sleep(5)
+                print('Sorry, this code has been already used')
+                time.sleep (5)
+            else:
+                pass
+        cv2.imshow ('Testing-code-scan', frame)
 
-        cv2.imshow('Testing-code-scan', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -34,17 +39,19 @@ def scan_barcode():
 
 def get_existing_data_from_sheets():
     scopes = [
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive'
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
     ]
 
     credentials = Credentials.from_service_account_file('creds.json',
-                                                        scopes=scopes
-                                                        )
+        scopes=scopes
+    )
 
     gc = gspread.authorize(credentials)
     existing_data = gc.open("batterydata").sheet1.get_all_values()
     df = pd.DataFrame(existing_data)
+    # df=df.rename(columns={"0":"TimeStamp","1":"BatteryCode","2":"BatteryType"})
+    print(df)
     return df
 
 def is_barcode_exists(barcode, dataframe):
@@ -52,35 +59,37 @@ def is_barcode_exists(barcode, dataframe):
 
 def add_data_to_sheets(data):
     scopes = [
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive'
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
     ]
 
     credentials = Credentials.from_service_account_file('creds.json',
-                                                        scopes=scopes
-                                                        )
+        scopes=scopes
+    )
 
     gc = gspread.authorize(credentials)
-    sheet = gc.open("batterydata").sheet1
+    sheet=gc.open("batterydata").sheet1
 
-    new_row = [pd.Timestamp.now().strftime("%d-%m-%Y %H:%M:%S"), data['Barcode']]
+    new_row = [pd.Timestamp.now().strftime("%d-%m-%Y %H:%M:%S"),data['Barcode']]
     sheet.append_row(new_row)
 
 def process_data(data, dataframe):
+    # dataframe = dataframe.append(data, ignore_index=True)
+    # add_data_to_sheets(data)
+    # print("Data added to Google Sheets and DataFrame.")
     if is_barcode_exists(data['Barcode'], dataframe):
-        st.warning("Barcode already exists.")
+        print("Barcode already exists.")
     else:
         dataframe = dataframe.append(data, ignore_index=True)
         add_data_to_sheets(data)
-        st.success("Data added to Google Sheets and DataFrame.")
+        print("Data added to Google Sheets and DataFrame.")
 
 def main():
-    st.title("Barcode Scanner App")
-    st.write("Click the button below to scan a barcode.")
-    if st.button("Scan Barcode"):
-        existing_data = get_existing_data_from_sheets()
-        barcode_data = scan_barcode()
-        process_data(barcode_data, existing_data)
+    existing_data = get_existing_data_from_sheets()
+    # print(existing_data)
+    barcode_data = scan_barcode()
+    process_data(barcode_data, existing_data)
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
